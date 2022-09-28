8ad56355aaceeb06e5211efc31495a55f9c1f833
joanaizabeldossantos <110937614+joanaizabeldossantos@users.noreply.github.com>
2022-09-28 16:54:08 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,38 @@
+  // alert ("oi! eu sou alert ");
+  // document.write(Estou na página .);
+  // console.log (" eu estou no console")
+   let tabuada = 2
+
+ function alo (){
+     document.write("<h1>tabuada do " + tabuada + "<h1>");
+     document.write (  tabuada" + x 1 = " + ( tabuada * 1)+"<br>");
+document.write (  tabuada" + x 2 = " + ( tabuada * 2)+"<br>");
+document.write (  tabuada" + x 3 = " + ( tabuada * 3)+"<br>");
+document.write (  tabuada" + x 4 = " + ( tabuada * 4)+"<br>");
+document.write (  tabuada" + x 5 = " + ( tabuada * 5)+"<br>");
+document.write (  tabuada" + x 6 = " + ( tabuada * 6)+"<br>");
+document.write (  tabuada" + x 7 = " + ( tabuada * 7)+"<br>");
+document.write (  tabuada" + x 8 = " + ( tabuada * 8)+"<br>");
+document.write (  tabuada" + x 9 = " + ( tabuada * 9)+"<br>");
+document.write (  tabuada" + x 10 = " + ( tabuada * 10));
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
